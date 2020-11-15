f49215af0024bd17beb0e287f4b2b84693421896
KyleCI <69059696+KyleCI@users.noreply.github.com>
2020-11-15 14:37:06 +0100

    google webscrapping

--- automation.py
+++ automation.py
@@ -7,11 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
-driver.get('Http://www.youtube.com')
+driver.get('Http://www.google.com')
 
-searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+searchbox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
 searchbox.click()
-searchbox.send_keys('Python tutorials')
+searchbox.send_keys('Python tutorials for automations')
 
-searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+searchbutton = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]')
 searchbutton.click()
